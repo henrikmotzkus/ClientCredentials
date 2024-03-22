da40b37365c6725763bc244b8cacf774ed9a4d57
henrikmotzkus <henrik@motzkus.info>
2024-03-22 09:18:21 +0100

    FileUpload Tests

--- Upload-FiletoSPviaGraph.ps1
+++ Upload-FiletoSPviaGraph.ps1
@@ -1,15 +1,26 @@
-# This script call the Microsoft Graph API to upload a file to a sharepoint site
+# This demo script calls the Microsoft Graph API to upload a file to a sharepoint site
 # You need a Service Principal in the AAD with application permissions
-# Files.ReadWrite.all
-# Sites.Read.All
+#
 # Sites.Selected
 #
 # Then you need to change the secrets.json file and fill in your secrets
+#
+# First prepare your app registration in order to upload files
+# https://techcommunity.microsoft.com/t5/microsoft-sharepoint-blog/develop-applications-that-use-sites-selected-permissions-for-spo/ba-p/3790476
+#
+#
+# You need the site ID first
+#
+#
+#
+
+
+
 function GetAccessToken {
     # Create a JSON with all the secrets 
     $file = Get-Content -Path .\Secrets.json | ConvertFrom-Json
     # AppID
-    $AppClientId= $file.AppClientId
+    $AppClientId = $file.AppClientId
     # Secret of the App. Never commit it to a code repository!
     $Secret = $file.Secret
     $TenantID = $file.TenantID
@@ -32,32 +43,38 @@ function GetAccessToken {
     return $AccessToken
 }
 
-function GetSiteID {
-    param (
-        [string]$Searchterm,
-        [string]$AccessToken
-    )
-    $headers = @{'Content-Type'="application\json";'Authorization'="Bearer $AccessToken"}
-    # Get site id from site. Please change the search term
-    $apiurl3 = "https://graph.microsoft.com/v1.0/sites"
-    $response3 = Invoke-RestMethod -Headers $headers -Uri $apiurl3 -Method Get
-    $array = $response3.value
-    $index = [Array]::IndexOf($array.name, $Searchterm)
-    #$index
-    $siteid = $array[$index].id
-    return $siteid
-}
+# Not used anymore
+<#
+    function GetSiteID {
+        param (
+            [string]$Searchterm,
+            [string]$AccessToken
+        )
+        $headers = @{'Content-Type'="application\json";'Authorization'="Bearer $AccessToken"}
+        # Get site id from site. Please change the search term
+        $apiurl3 = "https://graph.microsoft.com/v1.0/sites"
+        $response3 = Invoke-RestMethod -Headers $headers -Uri $apiurl3 -Method Get
+        $array = $response3.value
+        $index = [Array]::IndexOf($array.name, $Searchterm)
+        #$index
+        $siteid = $array[$index].id
+        return $siteid
+    }
+#>
 
 # Get content from site id
 function GetSiteContent {
     param (
-        [string]$sitename
+        [string]$SiteId
         )
     $AccessToken = GetAccessToken
-    $siteid = GetSiteID -Searchterm $sitename -AccessToken $AccessToken
+    #$sitename = "FileUpload"
+    #$siteid = GetSiteID -Searchterm $sitename -AccessToken $AccessToken
+    $siteid = "motzkus.sharepoint.com,3a3fbf16-19c9-4938-b37c-4ae52431c9e5,193021de-64a7-40dd-91f9-8e8f0effd11d"
     $apiurl5 = "https://graph.microsoft.com/v1.0/sites/$siteid/drive/items/root/children"
     $headers = @{'Content-Type'="application\json";'Authorization'="Bearer $AccessToken"}
     $response5 = Invoke-RestMethod -Headers $headers -Uri $apiurl5 -Method Get
+    $response5.value
     return $response5.value.name
 }
 
@@ -65,19 +82,18 @@ function GetSiteContent {
 function UploadFile {
     param (
         [string]$Path,
-        [string]$SiteName
+        [string]$SiteId
     )
     $AccessToken = GetAccessToken
     $headers6 = @{'Content-Type'="text/plain";'Authorization'="Bearer $AccessToken"}
-    $siteid = GetSiteID -Searchterm $SiteName -AccessToken $AccessToken 
     $filename = $Path.Substring(3)
     $apiurl6 = "https://graph.microsoft.com/v1.0/sites/$siteid/drive/items/root:/${filename}:/content"
     $response6 = Invoke-RestMethod -Headers $headers6 -Uri $apiurl6 -Method Put -InFile $Path
     return $response6.createdDateTime
 }
 
-UploadFile -Path "C:\Henrik-NonProfit-Engel.png" -SiteName "FileUpload"
-GetSiteContent -sitename "FileUpload"
+$file = Get-Content -Path .\Secrets.json | ConvertFrom-Json
+$siteid = $file.siteid
 
-
-
+GetSiteContent -SiteId $siteid
+UploadFile -Path "C:\Users\hemotzku\OneDrive - Microsoft\Desktop\Template.xlsx" -SiteId $siteid
